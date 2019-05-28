c6551368994dc2ea60c03c777df91825d650c64a
Emerson Bilieri Cluadelino <ebilieri@hotmail.com>
2019-05-27 23:02:42 -0300

    criação das entidades

---
+++ QuickBuy.Dominio/Entidades/Entidade.cs
@@ -0,0 +1,6 @@
+﻿namespace QuickBuy.Dominio.Entidades
+{
+    public abstract class Entidade
+    {
+    }
+}
---
+++ QuickBuy.Dominio/Entidades/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace QuickBuy.Dominio.Entidades
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
