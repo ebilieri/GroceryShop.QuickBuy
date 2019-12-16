aaf86e915a6fa9d3d1f60772ca5ac7df5e569c1e
tc.Claudelino <tc.Claudelino@ibopemedia.kantar.com>
2019-12-16 17:20:46 -0300

    ajuste produto component

--- QuickBuy.Angular/ClientApp/src/app/produto/produto.component.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ProdutoComponent = /** @class */ (function () {
-    function ProdutoComponent() {
-    }
-    return ProdutoComponent;
-}());
-exports.ProdutoComponent = ProdutoComponent;
-//# sourceMappingURL=produto.component.js.map
